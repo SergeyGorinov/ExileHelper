f675b5a07defd715d127832d1ad9f323e3ca3d8c
Sergey Gorinov <xpeHoc2@hotmail.com>
2020-07-08 18:00:12 +0300

    Add ExpandableRecyclerView

---
+++ app/src/main/java/com/example/poetradeapp/CurrencyListAdapter.kt
@@ -0,0 +1,91 @@
+package com.example.poetradeapp
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.thoughtbot.expandablerecyclerview.ExpandableRecyclerViewAdapter
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
+import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder
+import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder
+
+class CurrencyGroupViewHolder(itemView: View) : GroupViewHolder(itemView) {
+    private val currencyGroupId = itemView.findViewById<TextView>(R.id.CurrencyGroupId)
+    private val currencyGroupDescription =
+        itemView.findViewById<TextView>(R.id.CurrencyGroupDescription)
+
+    fun bindData(currencyGroup: MainActivity.CurrencyGroup) {
+        currencyGroupId.text = currencyGroup.id
+        currencyGroupDescription.text = currencyGroup.description
+    }
+//    override fun expand() {
+//        animate(true)
+//    }
+//
+//    override fun collapse() {
+//        animate(false)
+//    }
+//
+//    private fun animate(isExpand: Boolean) {
+//        val fromDegrees = if(isExpand) 360f else 180f
+//        val toDegrees = if(isExpand) 180f else 360f
+//        val rotate = RotateAnimation(fromDegrees, toDegrees, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f)
+//        rotate.duration = 300
+//        rotate.fillAfter = true
+//        currencyGroupId.animation = rotate
+//    }
+}
+
+class CurrencyViewHolder(itemView: View) : ChildViewHolder(itemView) {
+    private val currencyId = itemView.findViewById<TextView>(R.id.CurrencyId)
+    private val currencyDescription = itemView.findViewById<TextView>(R.id.CurrencyDescription)
+    private val currencyImage = itemView.findViewById<ImageView>(R.id.CurrencyImage)
+
+    fun bindData(currency: MainActivity.Currency) {
+        currencyId.text = currency.id
+        currencyDescription.text = currency.text
+        currencyImage.setImageBitmap(currency.image)
+    }
+}
+
+class CurrencyGroupListAdapter(val currencyGroups: MutableList<ExpandableGroup<*>?>?) :
+    ExpandableRecyclerViewAdapter<CurrencyGroupViewHolder, CurrencyViewHolder>(currencyGroups) {
+    override fun onCreateGroupViewHolder(
+        parent: ViewGroup?,
+        viewType: Int
+    ): CurrencyGroupViewHolder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.currency_group, parent, false)
+        return CurrencyGroupViewHolder(view)
+    }
+
+    override fun onCreateChildViewHolder(parent: ViewGroup?, viewType: Int): CurrencyViewHolder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.currency, parent, false)
+        return CurrencyViewHolder(view)
+    }
+
+    override fun onBindGroupViewHolder(
+        holder: CurrencyGroupViewHolder?,
+        flatPosition: Int,
+        group: ExpandableGroup<*>?
+    ) {
+        val currencyGroup = (group as MainActivity.CurrencyGroup)
+        holder?.bindData(currencyGroup)
+    }
+
+    override fun onBindChildViewHolder(
+        holder: CurrencyViewHolder?,
+        flatPosition: Int,
+        group: ExpandableGroup<*>?,
+        childIndex: Int
+    ) {
+        val currency = (group as MainActivity.CurrencyGroup).currencies[childIndex]
+        holder?.bindData(currency)
+    }
+
+    fun addData(item: ExpandableGroup<*>?) {
+        currencyGroups?.add(item)
+        this.notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/poetradeapp/MainActivity.kt
+++ app/src/main/java/com/example/poetradeapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.poetradeapp
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ListAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -21,36 +28,40 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    data class League (
+    data class League(
         val id: String,
         val text: String
     )
 
-    data class LeagueModel (
+    data class LeagueModel(
         val result: Array<League>
     )
 
-    data class StaticData (
+    data class StaticData(
         val id: String,
         val text: String,
         val image: String
     )
 
-    data class StaticEntries (
+    data class StaticEntries(
         val id: String,
         val label: String,
         val entries: Array<StaticData>
     )
 
-    data class StaticModel (
+    data class StaticModel(
         val result: Array<StaticEntries>
     )
 
-    class ViewModel (
+    class CurrencyGroup(val id: String, val description: String, val currencies: List<Currency>) :
+        ExpandableGroup<Currency>(id, currencies)
+
+    @Parcelize
+    class Currency(
         val id: String,
         val text: String,
         val image: Bitmap
-    )
+    ) : Parcelable
 
     private val httpClient = OkHttpClient()
     private val gson = Gson()
@@ -58,11 +69,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var items: Array<ViewModel> = arrayOf()
+
+        CurrencyGroupList.layoutManager = LinearLayoutManager(this)
+
+        CurrencyGroupList.adapter = CurrencyGroupListAdapter(mutableListOf<ExpandableGroup<*>?>())
 
         doAsync {
             try {
-                var request = Request.Builder().url("https://www.pathofexile.com/api/trade/data/leagues").build()
+                var request =
+                    Request.Builder().url("https://www.pathofexile.com/api/trade/data/leagues")
+                        .build()
                 httpClient.newCall(request).execute().use { response ->
                     if (!response.isSuccessful) throw IOException("Failed. Code: ${response.code()}")
                     var data = gson.fromJson<LeagueModel>(response.body()!!.string())
@@ -71,37 +87,42 @@ class MainActivity : AppCompatActivity() {
                             val adapter = CustomSpinnerAdapter(data.result, it)
                             LeagueList.adapter = adapter
                             LeagueList.onItemSelectedListener = adapter
-                        }
-                        catch (e: java.lang.Exception) {
+                        } catch (e: java.lang.Exception) {
                             println(e)
                         }
                     }
                 }
 
-                request = Request.Builder().url("https://www.pathofexile.com/api/trade/data/static").build()
+                request = Request.Builder().url("https://www.pathofexile.com/api/trade/data/static")
+                    .build()
                 httpClient.newCall(request).execute().use { response ->
                     if (!response.isSuccessful) throw IOException("Failed. Code: ${response.code()}")
                     val jsonData = response.body()!!.string()
                     var data = gson.fromJson<StaticModel>(jsonData)
 
-                    data.result.forEach { MainEntry ->
-                        MainEntry.entries.forEach { Entry ->
-                            val url = URL("https://www.pathofexile.com" + Entry.image)
+                    data.result.forEach { currencyGroup ->
+                        var currencies = listOf<Currency>()
+                        currencyGroup.entries.forEach { currency ->
+                            val url = URL("https://www.pathofexile.com" + currency.image)
                             val connection = url.openConnection() as HttpURLConnection
                             connection.doInput = true
                             connection.connect()
                             val imageStream = connection.inputStream
                             val image = BitmapFactory.decodeStream(imageStream)
-                            items += ViewModel(Entry.id, Entry.text, image)
+                            currencies += Currency(currency.id, currency.text, image)
                             connection.disconnect()
                         }
-                    }
-                    uiThread {
-                        StaticItemsList.adapter = CustomListViewAdapter(items, it)
+                        var group = CurrencyGroup(
+                            currencyGroup.id,
+                            currencyGroup.label,
+                            currencies
+                        )
+                        uiThread {
+                            (CurrencyGroupList.adapter as CurrencyGroupListAdapter).addData(group)
+                        }
                     }
                 }
-            }
-            catch (e: Exception) {
+            } catch (e: Exception) {
                 println(e.message)
             }
         }
